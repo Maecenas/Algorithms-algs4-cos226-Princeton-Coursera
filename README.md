87df4050925908939694e6b62c9a8b6ac0d27be9
Maecenas Li <lx70716@gmail.com>
2019-05-04 17:17:58 +0900

    docs: Update links and badges

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Algorithms-Princeton-Coursera
 
-<img src="http://algs4.cs.princeton.edu/cover.png" align=right hspace=10 width=150 alt = "Algorithms 4/e textbook">
+[![Open Source Society University - Computer Science](https://img.shields.io/badge/OSSU-computer--science-blue.svg)](https://github.com/ossu/computer-science)
 
-Also known as [algs4](http://algs4.cs.princeton.edu/) and Princeton [COS 226](http://www.princeton.edu/~cos226/), **Algorithms** on Coursera is an introductory course to fundamental data types, algorithms, and data structures, divided into two Parts.
+[<img src="http://algs4.cs.princeton.edu/cover.png" align=right hspace=10 width=150 alt="Algorithms 4/e textbook">](https://algs4.cs.princeton.edu/)
+
+Also known as [algs4](https://algs4.cs.princeton.edu/) and Princeton [COS 226](http://www.princeton.edu/~cos226/), **Algorithms** on Coursera is an introductory course to fundamental data types, algorithms, and data structures, divided into two Parts.
 
 - [Part I](https://www.coursera.org/learn/algorithms-part1/) focuses on elementary data structures, sorting, and searching. Topics include union−find, binary search, stacks, queues, bags, insertion sort, selection sort, shellsort, quicksort, 3-way quicksort, mergesort, heapsort, binary heaps, binary search trees, red−black trees, separate-chaining and linear-probing hash tables, Graham scan, and kd-trees.
 
--- src/main/java/assignment9/BoggleSolver.java
+++ src/main/java/assignment9/BoggleSolver.java
@@ -1,13 +1,15 @@
 package assignment9;
 
 import assignment9.utils.BoggleBoard;
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
 
 import java.util.ArrayList;
-import java.util.TreeSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class BoggleSolver {
 
@@ -15,7 +17,6 @@ public class BoggleSolver {
 
     private Node root; // R-Way Trie
     private boolean[] marked;
-    private TreeSet<String> words;
 
     /**
      * Initializes the data structure using the given array of strings as the dictionary
@@ -24,36 +25,50 @@ public class BoggleSolver {
      */
     public BoggleSolver(String[] dictionary) {
         for (String word : dictionary) {
-            root = put(root, word, 0);
+            int len = word.length(), score;
+
+            if (len <= 2) score = 0;
+            else if (len <= 4) score = 1;
+            else if (len == 5) score = 2;
+            else if (len == 6) score = 3;
+            else if (len == 7) score = 5;
+            else score = 11;
+
+            put(word, score);
         }
     }
 
     private static class Node {
-        private boolean isWord;
+        private int val;
         private Node[] next = new Node[R];
     }
 
-    private Node put(Node x, String key, int d) {
-        if (x == null) x = new Node();
-        if (d == key.length()) x.isWord = true;
-        else {
-            char c = key.charAt(d);
-            x.next[c - 'A'] = put(x.next[c - 'A'], key, d + 1);
-        }
-        return x;
+    private int get(String key) {
+        Node x = get(root, key, 0);
+        if (x == null) return 0;
+        return x.val;
     }
 
     private Node get(Node x, String key, int d) {
         if (x == null) return null;
         if (d == key.length()) return x;
-        char c = key.charAt(d);
-        return get(x.next[c - 'A'], key, d + 1);
+        int c = key.charAt(d) - 'A';
+        return get(x.next[c], key, d + 1);
     }
 
-    private boolean contains(String key) {
-        Node x = get(root, key, 0);
-        if (x == null) return false;
-        return x.isWord;
+    private void put(String key, int val) {
+        root = put(root, key, val, 0);
+    }
+
+    private Node put(Node x, String key, int val, int d) {
+        if (x == null) x = new Node();
+        if (d == key.length()) {
+            x.val = val;
+        } else {
+            int c = key.charAt(d) - 'A';
+            x.next[c] = put(x.next[c], key, val, d + 1);
+        }
+        return x;
     }
 
     /**
@@ -63,13 +78,14 @@ public class BoggleSolver {
      * @return the set of all valid words as an Iterable.
      */
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        words = new TreeSet<>();
-        int R = board.rows(), C = board.cols();
+        HashSet<String> words = new HashSet<>();
+        final int R = board.rows(), C = board.cols();
         if (R == 0 || C == 0) return words;
 
         // Calculate for once, passed as reference during DFS
-        final ArrayList<Iterable<Integer>> adj = new ArrayList<>(R * C);
+        final List<Iterable<Integer>> adj = new ArrayList<>(R * C);
         final char[] letters = new char[R * C];
+        final StringBuilder sb = new StringBuilder(32);
         marked = new boolean[R * C];
 
         // Calculate adj
@@ -78,15 +94,15 @@ public class BoggleSolver {
             for (int j = 0; j < C; j++, idx++) {
                 boolean hasLeft = j > 0, hasRight = j < C - 1;
 
-                Queue<Integer> q = new Queue<>();
-                if (hasUp && hasLeft) q.enqueue(idx - C - 1);
-                if (hasUp) q.enqueue(idx - C);
-                if (hasUp && hasRight) q.enqueue(idx - C + 1);
-                if (hasLeft) q.enqueue(idx - 1);
-                if (hasRight) q.enqueue(idx + 1);
-                if (hasDown && hasLeft) q.enqueue(idx + C - 1);
-                if (hasDown) q.enqueue(idx + C);
-                if (hasDown && hasRight) q.enqueue(idx + C + 1);
+                Bag<Integer> q = new Bag<>();
+                if (hasUp && hasLeft) q.add(idx - C - 1);
+                if (hasUp) q.add(idx - C);
+                if (hasUp && hasRight) q.add(idx - C + 1);
+                if (hasLeft) q.add(idx - 1);
+                if (hasRight) q.add(idx + 1);
+                if (hasDown && hasLeft) q.add(idx + C - 1);
+                if (hasDown) q.add(idx + C);
+                if (hasDown && hasRight) q.add(idx + C + 1);
 
                 adj.add(q);
                 letters[idx] = board.getLetter(i, j);
@@ -94,28 +110,31 @@ public class BoggleSolver {
         }
 
         for (int idx = 0; idx < R * C; idx++) {
-            dfs(adj, letters, idx, new StringBuilder(), root);
+            dfs(adj, words, letters, idx, sb, root);
         }
         return words;
     }
 
-    private void dfs(ArrayList<Iterable<Integer>> adj, char[] letters, int idx, StringBuilder pre, Node last) {
+    private void dfs(List<Iterable<Integer>> adj, Set<String> words, char[] letters, int idx, StringBuilder sb, Node last) {
         char c = letters[idx];
         Node now = last.next[c - 'A'];
         if (c == 'Q' && now != null) now = now.next['U' - 'A'];
         if (now == null) return;
-        if (c == 'Q') pre.append("QU");
-        else pre.append(c);
+        if (c == 'Q') sb.append("QU");
+        else sb.append(c);
 
-        if (pre.length() >= 3 && now.isWord) words.add(pre.toString());
+        if (now.val > 0) words.add(sb.toString());
 
         marked[idx] = true;
         for (int next : adj.get(idx)) {
             if (!marked[next]) {
-                dfs(adj, letters, next, new StringBuilder(pre), now);
+                dfs(adj, words, letters, next, sb, now);
             }
         }
         marked[idx] = false;
+        sb.deleteCharAt(sb.length() - 1);
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == 'Q')
+            sb.deleteCharAt(sb.length() - 1);
     }
 
     /**
@@ -125,16 +144,7 @@ public class BoggleSolver {
      * @return the score of the given word if it is in the dictionary, zero otherwise
      */
     public int scoreOf(String word) {
-        if (!contains(word)) return 0;
-
-        int len = word.length();
-
-        if (len <= 2) return 0;
-        else if (len <= 4) return 1;
-        else if (len == 5) return 2;
-        else if (len == 6) return 3;
-        else if (len == 7) return 5;
-        else return 11;
+        return get(word);
     }
 
     public static void main(String[] args) {
